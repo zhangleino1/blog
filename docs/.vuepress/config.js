85c1dff2a37310988063f1aa234152df81c10e39
zzzhanglei <851106500@qq.com>
2022-03-26 18:56:14 +0800

    修改标题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     // 站点配置
     lang: 'zh-CN',
     title: '程序员石磊的技术博客',
-    description: '云原生开发，在职非全日制，综合能源项目，物联网项目',
+    description: '云原生开发，在职非全日制，能耗检测软件，综合能源规划系统，能源运营及调度系统，物联网项目',
   
     // 主题和它的配置
     theme: '@vuepress/theme-default',
