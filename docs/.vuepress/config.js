6cd71a9b50dddd86dd3cf0ef55bcbf97d19971ad
zzzhanglei <851106500@qq.com>
2022-03-26 18:22:44 +0800

    提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 站点配置
+    lang: 'zh-CN',
+    title: '程序员石磊的技术博客',
+    description: '云原生开发，在职非全日制，综合能源项目，物联网项目',
+  
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+      logo: 'https://vuejs.org/images/logo.png',
+    },
+  }
